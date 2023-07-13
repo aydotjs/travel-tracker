023e2ef286e512fbec494f756531d7b092e9ab03
aydotjs <ayobami66@gmail.com>
2023-07-13 13:00:55 +0100

    second project almost done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /* 
 
 */
@@ -22,14 +24,39 @@ function Logo() {
   return <h1>🌴Far Away💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!description) return
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1)
+  }
+ 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>{num}</option>
+          <option value={num} key={num}>
+            {num}
+          </option>
         ))}
       </select>
+      <input
+        placeholder="item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
       <button>Add</button>
     </form>
   );
