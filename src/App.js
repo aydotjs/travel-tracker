27bf5565ed3a9cb6b1588e0346cc13be10f91b46
aydotjs <ayobami66@gmail.com>
2023-07-18 14:42:17 +0100

    lifting up state

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Grocery", quantity: 4, packed: false },
-  { id: 4, description: "Singlet", quantity: 5, packed: true },
-  { id: 5, description: "Underwear", quantity: 20, packed: false },
 ];
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => {
+      return [...items, item];
+    });
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stat />
     </div>
   );
@@ -23,23 +26,24 @@ export default function App() {
 function Logo() {
   return <h1>🌴Far Away💼</h1>;
 }
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    if(!description) return
+    if (!description) return;
     const newItem = {
       description,
       quantity,
       packed: false,
       id: Date.now(),
     };
-    console.log(newItem);
+    onAddItems(newItem);
     setDescription("");
-    setQuantity(1)
+    setQuantity(1);
   }
- 
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
@@ -61,11 +65,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
